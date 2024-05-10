65493a67db7f1267f6d603cd2be266733518134e
Kausar Hasan <kausarhsn92@gmail.com>
2024-05-10 21:25:49 +0600

    Pagination, Create new Enrollment

--- controllers/enrollmentController.js
+++ controllers/enrollmentController.js
@@ -3,8 +3,17 @@ const Enrollment = require('../models/enrollmentModel');
 // Controller function to create a new enrollment
 exports.createEnrollment = async (req, res) => {
   try {
-    const { user, course,userBkashNumber } = req.body;
-    const enrollment = new Enrollment({ user, course,userBkashNumber });
+    const { user, course, userBkashNumber } = req.body;
+
+    // Check if the user is already enrolled in the course
+    const existingEnrollment = await Enrollment.findOne({ user: user, course: course });
+    
+    if (existingEnrollment) {
+      return res.status(409).json({ message: 'User is already enrolled in this course' });
+    }
+
+    // Create a new enrollment if not already enrolled
+    const enrollment = new Enrollment({ user, course, userBkashNumber });
     await enrollment.save();  
     res.status(201).json(enrollment);
   } catch (error) {
@@ -12,11 +21,27 @@ exports.createEnrollment = async (req, res) => {
   }
 };
 
-// Controller function to get all enrollments
+// Controller function to get all enrollments with pagination and sorted by the latest date
 exports.getAllEnrollments = async (req, res) => {
   try {
-    const enrollments = await Enrollment.find();
-    res.json(enrollments);
+    // Default values
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10; // You can set your own default limit
+
+    const startIndex = (page - 1) * limit;
+    const total = await Enrollment.countDocuments(); // Total documents
+
+    const enrollments = await Enrollment.find()
+      .sort({ enrolledAt: -1 }) // Sorting by the enrolledAt date in descending order
+      .skip(startIndex)
+      .limit(limit);
+
+    res.json({
+      enrollments,
+      currentPage: page,
+      totalPages: Math.ceil(total / limit),
+      totalEnrollments: total
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- models/enrollmentModel.js
+++ models/enrollmentModel.js
@@ -4,7 +4,8 @@ const enrollmentSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: true,
+    index: true 
   },
   course: {
     type: mongoose.Schema.Types.ObjectId,
